dfce6b92fe4a3167637fbe525e9e91f46aa620cc
Nathan Freeman <nathanlfreeman@gmail.com>
2023-03-21 12:00:20 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,27 @@
+require("dotenv").config()
+
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT
+const bcrypt = require('bcrypt')
+
+app.set('view-engine', 'ejs')
+
+
+app.get('/', (req, res) => {
+    res.render('index.ejs', { name: "Nathan"}) 
+})
+
+app.get('/login', (req, res) => {
+    res.render('login.ejs') 
+})
+
+app.get('/register', (req, res) => {
+    res.render('register.ejs') 
+})
+
+
+
+app.listen(PORT, () => {
+    console.log('Express is listening on ' + PORT); 
+})
