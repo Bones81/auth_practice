319b11a684641fa6013faa2b07bc752584ba8213
Nathan Freeman <nathanlfreeman@gmail.com>
2023-03-21 12:38:02 -0400

    add some organizing comments

--- server.js
+++ server.js
@@ -1,23 +1,25 @@
+// DEPENDENCIES
 require("dotenv").config()
-
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
-const db = mongoose.connection
-
-const mongoLOC = 'mongodb://localhost:27017/'+'auth_practice'
-
-
-const PORT = process.env.PORT
 const bcrypt = require('bcrypt')
 
+//DATABASE SETUP
+const db = mongoose.connection
+//if this were a deployed app, there would be a .env value for a Mongo Atlas connection string
+//but since this is just practice, the localhost variant is fine
+const mongoLOC = 'mongodb://localhost:27017/'+'auth_practice'
+
+// VALUES FROM OTHER FILES
+const PORT = process.env.PORT
 const User = require('./models/users')
 
+// MIDDLEWARE
 app.set('view-engine', 'ejs')
-
 app.use(express.urlencoded({ extended: false }))
 
-
+//ROUTES
 app.get('/', (req, res) => {
     res.render('index.ejs', { name: "Nathan"}) 
 })
@@ -38,7 +40,7 @@ app.post('/register', (req, res) => {
      
 })
 
-
+// DB CHECKS
 db.on('error', e => console.log(e.message + ' ERROR is Mongod not running?'));
 db.on('connected', () => console.log('mongo connected: ', mongoLOC));
 db.on('disconnected', () => console.log('mongo disconnected'));
@@ -49,6 +51,7 @@ const connectToDB = async () => {
 }
 connectToDB()
 
+// SERVER LISTENER
 app.listen(PORT, () => {
     console.log('Express is listening on ' + PORT); 
 })
